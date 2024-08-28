77b790b786d6f20171eea75fc8c5e709fabafc13
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-08-28 13:55:05 +0100

    Added file reading and name == main

--- 2021/day_2.py
+++ 2021/day_2.py
@@ -1,1 +1,11 @@
 """Day 2 of the advent of code 2021"""
+
+
+def read_input(filename: str) -> list[str]:
+    """Reads the data from the file given"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return f.readlines()
+
+
+if __name__ == "__main__":
+    pass
