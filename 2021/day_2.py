269acadb5aa52023d7c0a6637572917dae34780d
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-08-28 14:05:29 +0100

    Day 2 part 1 finished

--- 2021/day_2.py
+++ 2021/day_2.py
@@ -7,5 +7,24 @@ def read_input(filename: str) -> list[str]:
         return f.readlines()
 
 
+def plot_course(data: list[str]) -> dict:
+    """Runs the course given from the data and returns the final
+    horizontal position and depth"""
+    positions = {"hor": 0, "depth": 0}
+
+    for move in data:
+        if "forward" in move:
+            positions["hor"] += int(move.strip()[-1])
+        elif "up" in move:
+            positions["depth"] -= int(move.strip()[-1])
+        else:
+            positions["depth"] += int(move.strip()[-1])
+
+    return positions
+
+
 if __name__ == "__main__":
-    pass
+    course = read_input("day_2_data.txt")
+    final_positions = plot_course(course)
+    print("The product of the final positions is " +
+          f"{final_positions["hor"] * final_positions["depth"]}")
