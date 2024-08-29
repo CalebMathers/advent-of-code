9d080248baacab901f98d5b8df134cb723c643f9
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-08-29 14:59:38 +0100

    Day 2 part 2 finished

--- 2015/day_2.py
+++ 2015/day_2.py
@@ -11,12 +11,13 @@ def calculate_paper(gift_dimensions: str) -> int:
     """Takes the dimensions of a gift and returns the square feet
     of wrapping paper required"""
     dimensions = gift_dimensions.strip().split("x")
+    dimensions = [int(num) for num in dimensions]
     if len(dimensions) != 3:
         raise ValueError("The dimensions string must contain 3 measurements.")
 
-    length_width = int(dimensions[0]) * int(dimensions[1])
-    width_height = int(dimensions[1]) * int(dimensions[2])
-    height_length = int(dimensions[0]) * int(dimensions[2])
+    length_width = dimensions[0] * dimensions[1]
+    width_height = dimensions[1] * dimensions[2]
+    height_length = dimensions[0] * dimensions[2]
 
     paper_needed = (2 * length_width) + \
         (2 * width_height) + (2 * height_length)
@@ -35,6 +36,35 @@ def calculate_total_paper(all_gift_dimensions: list[str]) -> int:
     return total
 
 
+def calculate_ribbon(gift_dimensions: str) -> int:
+    """Takes the dimensions of a gift and returns the total
+    feet of ribbon required"""
+    dimensions = gift_dimensions.strip().split("x")
+    dimensions = [int(num) for num in dimensions]
+    if len(dimensions) != 3:
+        raise ValueError("The dimensions string must contain 3 measurements.")
+
+    ribbon_needed = dimensions[0] * dimensions[1] * dimensions[2]
+    dimensions.sort()
+    ribbon_needed += (dimensions[0] * 2) + (dimensions[1] * 2)
+
+    return ribbon_needed
+
+
+def calculate_total_ribbon(all_gift_dimensions: list[str]) -> int:
+    """Calculates the total ribbon required"""
+    total = 0
+
+    for gift in all_gift_dimensions:
+        total += calculate_ribbon(gift)
+
+    return total
+
+
 if __name__ == "__main__":
     data = read_input("day_2_data.txt")
-    print(calculate_total_paper(data))
+    total_paper = calculate_total_paper(data)
+    total_ribbon = calculate_total_ribbon(data)
+
+    print(f"The total paper needed is: {total_paper}")
+    print(f"The total ribbon needed is {total_ribbon}")
--- 2015/test_day_2.py
+++ 2015/test_day_2.py
@@ -1,7 +1,7 @@
 """Tests for day 2"""
 
 import pytest
-from day_2 import calculate_paper, calculate_total_paper
+from day_2 import calculate_paper, calculate_total_paper, calculate_ribbon, calculate_total_ribbon
 
 
 def test_calculate_paper_valid_inputs_one():
@@ -31,3 +31,25 @@ def test_calculate_total_paper():
     dimensions = ["2x3x4", "1x1x10"]
     res = calculate_total_paper(dimensions)
     assert res == 101
+
+
+def test_calculate_ribbon_valid_inputs_one():
+    """Test that calculate ribbon returns the correct number
+    with valid inputs"""
+    res = calculate_ribbon("2x3x4")
+    assert res == 34
+
+
+def test_calculate_ribbon_valid_inputs_two():
+    """Test that calculate ribbon returns the correct number
+    with valid inputs"""
+    res = calculate_ribbon("1x1x10")
+    assert res == 14
+
+
+def test_calculate_total_ribbon_valid_inputs():
+    """Tests that the total amount of ribbon is correct
+    with valid inputs"""
+    dimensions = ["2x3x4", "1x1x10"]
+    res = calculate_total_ribbon(dimensions)
+    assert res == 48
