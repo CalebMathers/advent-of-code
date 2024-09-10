ec1565b803c9f340e874b6b4734442b62498c21f
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-09-10 13:41:02 +0100

    day 1 2015 done

---
+++ 2015/day_1.py
@@ -0,0 +1,27 @@
+"""Day for for advent of code 2015"""
+
+
+def read_input(filename: str) -> list[str]:
+    """Reads the data from the file given."""
+    with open(filename, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+def get_final_destination(movements: str) -> int:
+    """Returns the final floor after a serious of movements given."""
+    return movements.count("(") - movements.count(")")
+
+
+def get_basement_index(movements: str) -> int:
+    """Returns the index in which the basement is entered."""
+    for i in range(len(movements)):
+        if movements[:i].count(")") > movements[:i].count("("):
+            return i
+
+    return None
+
+
+if __name__ == "__main__":
+    floor_movements = read_input("day_1_data.txt")
+    print(get_final_destination(floor_movements))
+    print(get_basement_index(floor_movements))
