37e3f22fc91407d49045d73348bed65f09dadd91
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-09-22 10:02:31 +0100

    Set up file for day 8

---
+++ 2015/day_8.py
@@ -0,0 +1,11 @@
+"""Day 8 for the advent of code 2015"""
+
+
+def read_input(filename: str) -> list[str]:
+    """Reads the data from the file given"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return [line.strip() for line in f.readlines()]
+
+
+if __name__ == "__main__":
+    pass
