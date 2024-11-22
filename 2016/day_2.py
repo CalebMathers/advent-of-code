9f40045045c3e492480f36515fdbc1ec962cd49f
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-11-22 11:45:33 +0000

    Day 2 complete

--- 2016/day_2.py
+++ 2016/day_2.py
@@ -97,8 +97,33 @@ def new_move_right(current_num: str) -> str:
     return left_movements[current_num]
 
 
+def new_find_code(instructions: list[str]) -> str:
+    """Conducts the movements for each instruction line and returns
+    the keypad code."""
+    code = ""
+    current_num = '5'
+
+    for instruction in instructions:
+        for movement in instruction:
+            if movement == "U":
+                current_num = new_move_up(current_num)
+            elif movement == "D":
+                current_num = new_move_down(current_num)
+            elif movement == "L":
+                current_num = new_move_left(current_num)
+            else:
+                current_num = new_move_right(current_num)
+
+        code += current_num
+
+    return code
+
+
 if __name__ == "__main__":
     movements_data = read_input("data/day_2_data.txt")
 
     part_one_code = find_code(movements_data)
     print(f"The code for the first keypad is {part_one_code}")
+
+    part_two_code = new_find_code(movements_data)
+    print(f"The code for the first keypad is {part_two_code}")
