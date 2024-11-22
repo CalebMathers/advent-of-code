e953379efd70bcd21672dd3cd482061e86f597f7
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-11-22 10:42:50 +0000

    Wrote functions to move keypad pointer

--- 2016/day_2.py
+++ 2016/day_2.py
@@ -1,8 +1,37 @@
 """Day 2 of the advent of code 2016."""
 
-KEYPAD = {(0, 0): 1, (0, 1): 2, (0, 2): 3,
-          (1, 0): 4, (1, 1): 5, (1, 2): 6,
-          (2, 0): 7, (2, 1): 8, (2, 2): 9}
+
+def move_up(current_num: int) -> int:
+    """"""
+    if current_num in [1, 2, 3]:
+        return current_num
+
+    return current_num - 3
+
+
+def move_down(current_num: int) -> int:
+    """"""
+    if current_num in [7, 8, 9]:
+        return current_num
+
+    return current_num + 3
+
+
+def move_left(current_num: int) -> int:
+    """"""
+    if current_num in [1, 4, 7]:
+        return current_num
+
+    return current_num - 1
+
+
+def move_right(current_num: int) -> int:
+    """"""
+    if current_num in [3, 6, 9]:
+        return current_num
+
+    return current_num + 1
+
 
 if __name__ == "__main__":
     pass
