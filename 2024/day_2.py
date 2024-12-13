336b96348af298c08873ae753d076ecfcfa6b9cf
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-12-13 11:01:42 +0000

    Part one finished

--- 2024/day_2.py
+++ 2024/day_2.py
@@ -13,18 +13,35 @@ def convert_list(reports: list[str]) -> list[list[int]]:
     return [[int(num) for num in level.split(" ")] for level in reports]
 
 
-def determine_line_safety(levels: list[int]) -> bool:
+def determine_report_safety(levels: list[int]) -> bool:
     """Returns true if the line is safe, else false."""
-    pass
+
+    # check for all increasing by 1 to 3
+    if (all(levels[i] > levels[i + 1] for i in range(len(levels) - 1))
+            and all(abs(levels[i] - levels[i + 1]) in [1, 2, 3] for i in range(len(levels) - 1))):
+        return True
+
+    # check for all decreasing by 1 to 3
+    if (all(levels[i] < levels[i + 1] for i in range(len(levels) - 1))
+            and all(abs(levels[i] - levels[i + 1]) in [1, 2, 3] for i in range(len(levels) - 1))):
+        return True
+
+    return False
 
 
 def find_total_safe(reports: list[list[int]]) -> int:
     """Returns the total number of safe reports."""
-    pass
+    safe_count = 0
+
+    for report in reports:
+        if determine_report_safety(report):
+            safe_count += 1
+
+    return safe_count
 
 
 if __name__ == "__main__":
     reports_input = (read_input("data/day_2_data.txt"))
     converted_reports = convert_list(reports_input)
 
-    print(converted_reports)
+    print(find_total_safe(converted_reports))
