44408fcda820cad715ab56f64b078cc3940879d0
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-12-13 10:30:11 +0000

    Set up files for day 2

---
+++ 2024/day_2.py
@@ -0,0 +1,11 @@
+"""Day 2 of advent of code 2024."""
+
+
+def read_input(filename: str) -> list[str]:
+    """Reads the data from the file given"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return [line.strip() for line in f.readlines()]
+
+
+if __name__ == "__main__":
+    pass
