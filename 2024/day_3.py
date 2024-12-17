ee1a448d0b07ebf6964a22028cda989c2d71d10d
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-12-17 14:42:00 +0000

    Part two complete

--- 2024/day_3.py
+++ 2024/day_3.py
@@ -31,9 +31,32 @@ def carry_out_multiplications(uncorrupted_calls: list[str]) -> int:
     return total
 
 
+def find_enabled_data(corrupted_data: str) -> str:
+    """Returns a list of all the enabled data 
+    from a string containing corrupted data."""
+    enable_pattern = r"do\(\)"
+    disable_pattern = r"don't\(\)"
+
+    do_split = re.split(enable_pattern, corrupted_data)
+    enabled_instructions = ""
+
+    for do_section in do_split:
+        enabled_instructions += re.split(disable_pattern,
+                                         do_section, maxsplit=1)[0]
+
+    return enabled_instructions
+
+
 if __name__ == "__main__":
     raw_data = read_input("data/day_3_data.txt")
 
     uncorrupted_data = find_uncorrupted_instructions(raw_data)
     total_result = carry_out_multiplications(uncorrupted_data)
     print(f"The total of all part one multiplications is: {total_result}")
+
+    enabled_data = find_enabled_data(raw_data)
+    uncorrupted_data_part_two = find_uncorrupted_instructions(enabled_data)
+    total_result_part_two = carry_out_multiplications(
+        uncorrupted_data_part_two)
+    print(f"""The total of all part two multiplications is: {
+          total_result_part_two}""")
