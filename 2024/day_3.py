e53a4b1019330e6ebf9168a30843ec1fb98bbccc
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-12-17 14:02:04 +0000

    Wrote function to read data and imported regex

--- 2024/day_3.py
+++ 2024/day_3.py
@@ -1,4 +1,14 @@
 """Day 3 of advent of code 2024."""
 
+import re
+
+
+def read_input(filename: str) -> list[str]:
+    """Reads the data from the file given"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return f.read()
+
+
 if __name__ == "__main__":
-    pass
+    raw_data = read_input("data/day_3_data.txt")
+    print(raw_data)
