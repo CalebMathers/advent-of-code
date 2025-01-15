dda747730f1f3db9bdbb408d1f31540e0637c97f
CalebMathers <caleb-mathers@hotmail.co.uk>
2025-01-15 11:39:15 +0000

    Wrote function for reading data

--- 2024/day_4.py
+++ 2024/day_4.py
@@ -1,4 +1,11 @@
 """Day 4 of advent of code 2024."""
 
+
+def read_input(filename: str) -> list[str]:
+    """Reads the data from the file given."""
+    with open(filename, "r", encoding="utf-8") as f:
+        return [line.strip() for line in f.readlines()]
+
+
 if __name__ == "__main__":
-    pass
+    word_search = read_input("data/day_4_data.txt")
