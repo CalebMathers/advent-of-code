4bbd95a0fa9946218db761012a0a5e10b8fcf0ab
CalebMathers <caleb-mathers@hotmail.co.uk>
2025-01-16 15:56:04 +0000

    Solved part two

--- 2024/day_4.py
+++ 2024/day_4.py
@@ -1,4 +1,4 @@
-# pylint: disable=C0200
+# pylint: disable=C0200,C0103
 
 """Day 4 of advent of code 2024."""
 
@@ -85,8 +85,38 @@ def get_words(word_search: list[str], row_index: int, char_index: int) -> list[s
     return [word for word in results if word == 'XMAS']
 
 
+def is_cross_word(word_search: list[str], row_index: int, char_index: int) -> bool:
+    """Returns true if the letter at the given index makes an X-MAS, else false."""
+    top_left = word_search[row_index - 1][char_index - 1]
+    top_right = word_search[row_index - 1][char_index + 1]
+    bot_left = word_search[row_index + 1][char_index - 1]
+    bot_right = word_search[row_index + 1][char_index + 1]
+
+    if ((top_left == "M" and bot_right == "S") or (top_left == "S" and bot_right == "M")):
+        if ((top_right == "M" and bot_left == "S") or (top_right == "S" and bot_left == "M")):
+            return True
+
+    return False
+
+
+def find_cross_word(word_search: list[str]) -> int:
+    """Return the total count of the word crossed in the grid of characters."""
+    total_count = 0
+
+    for row_num in range(1, len(word_search) - 1):
+        for char_num in range(1, len(word_search[row_num]) - 1):
+            if word_search[row_num][char_num] == "A":
+                if is_cross_word(word_search, row_num, char_num):
+                    total_count += 1
+
+    return total_count
+
+
 if __name__ == "__main__":
     word_search_data = read_input("data/day_4_data.txt")
-    word_count = find_word(word_search_data)
 
+    word_count = find_word(word_search_data)
     print(f"There are {word_count} occurrences of the word XMAS for part one.")
+
+    cross_word_count = find_cross_word(word_search_data)
+    print(f"There are {cross_word_count} occurrences of 'X-MAS' for part two.")
